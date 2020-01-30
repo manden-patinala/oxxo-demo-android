f7014cf6b77a4bab1e99e67a51a0750ff6ca788a
Armando Tello <armando.tello@terc.terra.com>
2020-01-30 17:43:52 -0600

    Initial project structure

---
+++ app/src/main/java/com/patinala/oxxodemo/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.patinala.oxxodemo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.patinala.oxxodemo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
